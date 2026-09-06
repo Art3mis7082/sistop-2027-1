c205b1e5b43d98c169eee4f0cea4d02fee2be60a
Angel David <angeldlopez0803@gmail.com.com>
2026-09-06 16:48:43 -0600

    Volvemos al saludo clasico y simple

--- practicas/3/LópezÁngel/saludo.go
+++ practicas/3/LópezÁngel/saludo.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "os"
-)
+import "fmt"
 
 func main() {
-    // Si hay más de un argumento, el índice 1 es el nombre que pasó el usuario
-    if len(os.Args) > 1 {
-        fmt.Printf("¡Hola, %s!\n", os.Args[1])
-    } else {
-        fmt.Println("¡Hola mundo!")
-    }
+    fmt.Println("¡Hola mundo!")
 }
