61777214f82e0ee232ebe5897883e80bc7966a2a
EclipseDR3 <line_dark031001@hotmail.com>
2026-09-07 23:12:46 -0600

    Editando programa de practica 3

--- practicas/3/programa.py
+++ practicas/3/programa.py
@@ -1,1 +1,11 @@
-print("¡Hola, mundo!")
+import sys
+
+def main():
+    if len(sys.argv) > 1:
+        nombre = sys.argv[1]
+    else:
+        nombre = "invitado"
+    print(f"¡Hola, {nombre}! Bienvenido.")
+
+if __name__ == "__main__":
+    main()
